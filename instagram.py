bc013d49cc55bdb9e24fc3c2f7a030c607ac7252
Subhrajit Prusty <subhrajit1997@gmail.com>
2017-08-21 23:56:35 +0530

    Update and rename insta_all.py to instagram.py

--- instagram.py
+++ instagram.py
@@ -3,22 +3,13 @@
 import os
 from bs4 import BeautifulSoup as bs4
 from selenium import webdriver
-import time
 
 username = input("Enter username (must be public) : ")
 link = "https://www.instagram.com/"+username
 
-
 browser = webdriver.Firefox(executable_path="/usr/bin/geckodriver")
 
 browser.get(link)
-# linkElem = browser.find_element_by_link_text("Load more")
-# print(type(linkElem))
-# linkElem.click()
-
-#for x in range(0,4):
-#	browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#	time.sleep(3)
 
 html = browser.page_source
 soup = bs4(html,"lxml")
